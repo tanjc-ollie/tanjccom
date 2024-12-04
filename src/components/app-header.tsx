548f7692e2d324aed2190af68957aa2639af27b8
tanjc-ollie <tanjc.ollie@gmail.com>
2024-12-04 14:56:01 -0500

    modified main page

--- src/components/app-header.tsx
+++ src/components/app-header.tsx
@@ -4,7 +4,7 @@ export default function AppHeader() {
   return (
     <header>
       <div className="flex flex-row w-full p-3 items-center align-middle space-x-5">
-        <h1 className="text-2xl font-bold italic">
+        <h1 className="text-md md:text-2xl font-bold italic">
           Ollie Tan | Senior Software Developer
         </h1>
         <LinekdinContact></LinekdinContact>
